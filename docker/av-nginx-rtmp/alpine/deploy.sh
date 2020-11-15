53ef3a747aa5228921189e168a3baf92a04e99cf
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-15 15:11:14 +0100

    add nginx rtmp

--- docker/av-nginx-rtmp/alpine/deploy.sh
+++ docker/av-nginx-rtmp/alpine/deploy.sh
@@ -1,6 +1,6 @@
 # build image
 docker build -t flecoqui/av-nginx-rtmp-alpine .
 # run image
-docker run  flecoqui/av-nginx-rtmp-alpine
+docker run -it  flecoqui/av-nginx-rtmp-alpine 
 #sample convert incoming rtsp stream into rtmp
 docker run flecoqui/av-nginx-rtmp-alpine
