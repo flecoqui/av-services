818c88f36888e2f2d6ab080277cc5a6d6e8ba08b
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-01-05 23:19:23 +0100

    add document for docker container

---
+++ app-edge/modules/av-rtmp-sim-alpine/docker-entrypoint-av-rtmp-sim.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+while [ : ]
+do
+ffmpeg -v verbose  -re -stream_loop -1 -i camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb   -f flv $RTMP_URL
+sleep 5
+done
+
--- docker/av-ffmpeg/alpine/deploy.sh
+++
@@ -1,6 +0,0 @@
-# build image
-docker build -t flecoqui/av-ffmpeg-alpine .
-# run image
-docker run  flecoqui/av-ffmpeg-alpine
-#sample convert incoming rtsp stream into rtmp
-docker run flecoqui/av-ffmpeg-alpine -v verbose -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov -strict -2 -c:a aac -b:a 192k -ar 44100 -r 30 -g 60 -keyint_min 60 -b:v 2000000 -c:v libx264 -preset veryfast  -profile main -level 3.0 -pix_fmt yuv420p -bufsize 1800k -maxrate 400k    -f flv rtmp://172.17.0.2:1935/live/stream
