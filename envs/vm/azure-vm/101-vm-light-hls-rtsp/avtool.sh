0e0a802ad1a4b7b98588b882099ea1c051341467
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-02-10 21:24:13 +0100

    update avtool.sh for vm

--- envs/vm/azure-vm/101-vm-light-hls-rtsp/avtool.sh
+++ envs/vm/azure-vm/101-vm-light-hls-rtsp/avtool.sh
@@ -53,16 +53,42 @@ if [[ ! $action == install && ! $action == start && ! $action == stop && ! $acti
     usage
     exit 1
 fi
+RESOURCE_GROUP=av-rtmp-rtsp-hls-vm-rg
+RESOURCE_REGION=eastus2
+AV_RTMP_PORT=1935
+AV_RTMP_PATH=live/stream
+AV_PREFIXNAME=rtmprtsphls
+AV_VMNAME="$AV_PREFIXNAME"vm
+AV_HOSTNAME="$AV_VMNAME"."$RESOURCE_REGION".cloudapp.azure.com
+AV_CONTAINERNAME=avchunks
+AV_LOGIN=vmadmn
+AV_PASSWORD=P@ssw0rd!
 # Check if configuration file exists
 if [[ ! -f "$repoRoot"/"$configuration_file" ]]; then
     cat > "$repoRoot"/"$configuration_file" << EOF
-RESOURCE_GROUP=av-vm-rg
-RESOURCE_REGION=eastus2
+RESOURCE_GROUP=${RESOURCE_GROUP}
+RESOURCE_REGION=${RESOURCE_REGION}
+AV_RTMP_PORT=${AV_RTMP_PORT}
+AV_RTMP_PATH=${AV_RTMP_PATH}
+AV_PREFIXNAME=${AV_PREFIXNAME}
+AV_VMNAME=${AV_VMNAME}
+AV_HOSTNAME=${AV_HOSTNAME}
+AV_CONTAINERNAME=${AV_CONTAINERNAME}
+AV_LOGIN=${AV_LOGIN}
+AV_PASSWORD=${AV_PASSWORD}
 EOF
 fi
 # Read variables in configuration file
 export $(grep RESOURCE_GROUP "$repoRoot"/"$configuration_file")
 export $(grep RESOURCE_REGION "$repoRoot"/"$configuration_file")
+export $(grep AV_RTMP_PORT "$repoRoot"/"$configuration_file")
+export $(grep AV_RTMP_PATH "$repoRoot"/"$configuration_file")
+export $(grep AV_PREFIXNAME "$repoRoot"/"$configuration_file")
+export $(grep AV_VMNAME "$repoRoot"/"$configuration_file")
+export $(grep AV_HOSTNAME "$repoRoot"/"$configuration_file")
+export $(grep AV_CONTAINERNAME "$repoRoot"/"$configuration_file")
+export $(grep AV_LOGIN "$repoRoot"/"$configuration_file")
+export $(grep AV_PASSWORD "$repoRoot"/"$configuration_file")
 
 
 if [[ "${action}" == "install" ]] ; then
@@ -73,30 +99,42 @@ if [[ "${action}" == "install" ]] ; then
     sudo apt-get -y update
     sudo apt-get -y install ffmpeg
     echo "Downloading content"
-    wget 
+    wget https://github.com/flecoqui/av-services/blob/main/content/camera-300s.mkv
     exit 0
 fi
 
 if [[ "${action}" == "deploy" ]] ; then
-    echo "Deploying service"
+    echo "Deploying service..."
+    az group create -n ${RESOURCE_GROUP} -l ${RESOURCE_REGION}
+    az group deployment create -g ${RESOURCE_GROUP} -n ${RESOURCE_GROUP}dep --template-file azuredeploy.json --parameters namePrefix=${AV_PREFIXNAME} vmAdminUsername=${} vmAdminPassword=${} rtmpPath=${AV_RTMP_PATH} containerName=${AV_CONTAINERNAME} --verbose -o json
+    echo "Deployment done"
     exit 0
 fi
 
 if [[ "${action}" == "undeploy" ]] ; then
-    echo "Undeploying service"
+    echo "Undeploying service..."
+    az group delete -n ${RESOURCE_GROUP} --yes
+    echo "Undeployment done"
     exit 0
 fi
 
 if [[ "${action}" == "start" ]] ; then
-    echo "Starting service"
+    echo "Starting service..."
+    az vm stop -n ${AV_VMNAME} -g ${RESOURCE_GROUP} 
+    echo "Start done"
     exit 0
 fi
 
 if [[ "${action}" == "stop" ]] ; then
-    echo "Stopping service"
+    echo "Stopping service..."
+    az vm stop -n ${AV_VMNAME} -g ${RESOURCE_GROUP} 
+    az vm deallocate -n ${AV_VMNAME} -g ${RESOURCE_GROUP} 
+    echo "Stop done"
     exit 0
 fi
 if [[ "${action}" == "test" ]] ; then
-    echo "Testing service"
+    echo "Testing service..."
+    ffmpeg -v verbose  -re -stream_loop -1 -i ./camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb -f flv rtmp://${AV_HOSTNAME}:${AV_RTMP_PORT}/${AV_RTMP_PATH}
+    echo "Test done"
     exit 0
 fi
