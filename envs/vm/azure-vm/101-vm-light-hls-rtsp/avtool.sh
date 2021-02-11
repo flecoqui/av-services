6bed70e05f6116b8a6e9954f0012673c2e12ae00
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-02-11 14:03:59 +0100

    update bash script

--- envs/vm/azure-vm/101-vm-light-hls-rtsp/avtool.sh
+++ envs/vm/azure-vm/101-vm-light-hls-rtsp/avtool.sh
@@ -88,6 +88,8 @@ AV_HOSTNAME=${AV_HOSTNAME}
 AV_CONTAINERNAME=${AV_CONTAINERNAME}
 AV_LOGIN=${AV_LOGIN}
 AV_PASSWORD=${AV_PASSWORD}
+AV_SASTOKEN=
+AV_STORAGENAME=
 EOF
 fi
 # Read variables in configuration file
@@ -99,6 +101,8 @@ export $(grep AV_PREFIXNAME "$repoRoot"/"$configuration_file")
 export $(grep AV_VMNAME "$repoRoot"/"$configuration_file")
 export $(grep AV_HOSTNAME "$repoRoot"/"$configuration_file")
 export $(grep AV_CONTAINERNAME "$repoRoot"/"$configuration_file")
+export $(grep AV_STORAGENAME "$repoRoot"/"$configuration_file")
+export $(grep AV_SASTOKEN "$repoRoot"/"$configuration_file")
 export $(grep AV_LOGIN "$repoRoot"/"$configuration_file")
 export $(grep AV_PASSWORD "$repoRoot"/"$configuration_file")
 
@@ -129,6 +133,11 @@ if [[ "${action}" == "deploy" ]] ; then
     az account show --output table --query  "{subscriptionId:id,tenantId:tenantId}"
     az group create -n ${RESOURCE_GROUP} -l ${RESOURCE_REGION}
     az deployment group create -g ${RESOURCE_GROUP} -n ${RESOURCE_GROUP}dep --template-file azuredeploy.json --parameters namePrefix=${AV_PREFIXNAME} vmAdminUsername=${AV_LOGIN} vmAdminPassword=${AV_PASSWORD} rtmpPath=${AV_RTMP_PATH} containerName=${AV_CONTAINERNAME} --verbose -o json
+    outputs=$(az deployment group show --name ${RESOURCE_GROUP}dep  -g ${RESOURCE_GROUP} --query properties.outputs)
+    AV_STORAGENAME=$(jq -r .storageAccount.value <<< $outputs)
+    AV_SASTOKEN=$(jq -r .storageSasToken.value <<< $outputs)
+    sed -i 's/AV_STORAGENAME=.*/AV_STORAGENAME=${AV_STORAGENAME}/' "$repoRoot"/"$configuration_file"
+    sed -i 's/AV_SASTOKEN=.*/AV_SASTOKEN=${AV_SASTOKEN}/' "$repoRoot"/"$configuration_file"    
     echo "Deployment done"
     exit 0
 fi
@@ -165,22 +174,12 @@ if [[ "${action}" == "test" ]] ; then
     rm -f testhls*.mp4
     rm -f testrtsp*.mp4
     rm -f testazure.xml   
-    az storage blob delete-batch -s ${AV_CONTAINERNAME} --account-name rtmprtsp3ahnc4mybfacssa --pattern *.mp4 
+    az storage blob delete-batch -s ${AV_CONTAINERNAME} --account-name ${AV_STORAGENAME} --pattern "*.mp4" --sas-token ${AV_SASTOKEN}
     echo "Testing service..."
     echo "RTMP Streaming command: ffmpeg -nostats -loglevel 0 -re -stream_loop -1 -i ./camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb -f flv rtmp://${AV_HOSTNAME}:${AV_RTMP_PORT}/${AV_RTMP_PATH}"
     ffmpeg -nostats -loglevel 0 -re -stream_loop -1 -i ./camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb -f flv rtmp://${AV_HOSTNAME}:${AV_RTMP_PORT}/${AV_RTMP_PATH} &
     #jobs
     sleep 10
-    echo "Testing chunks on Azure Storage..."
-    
-    wget -O testazure.xml "https://rtmprtsp3ahnc4mybfacssa.blob.core.windows.net/avchunks?sv=2015-04-05&ss=b&srt=sco&sp=rwdlac&st=2020-01-01T00%3A00%3A01.0000000Z&se=2030-01-01T00%3A00%3A01.0000000Z&spr=https&sig=YhxotWahYYTKVfLxP93f8WWWQrZYDtrEj063Fui0Ot8%3D&comp=list&restype=container"
-    blobs=($(grep -oP '(?<=Name>)[^<]+' "./testazure.xml"))
-
-    for i in ${!blobs[*]}
-    do
-    echo "$i" "${blobs[$i]}"
-    # instead of echo use the values to send emails, etc
-    done
 
     echo "Testing output RTMP..."
     echo "Output RTMP: rtmp://${AV_HOSTNAME}:${AV_RTMP_PORT}/${AV_RTMP_PATH}"
@@ -213,6 +212,26 @@ if [[ "${action}" == "test" ]] ; then
         kill %1
         exit 0
     fi
+    echo "Testing output RTSP successful"
+
+    echo "Testing output on Azure Storage..."    
+    wget -O testazure.xml "https://${AV_STORAGENAME}.blob.core.windows.net/${AV_CONTAINERNAME}?${AV_SASTOKEN}"
+    blobs=($(grep -oP '(?<=Name>)[^<]+' "./testazure.xml"))
+    bloblens=($(grep -oP '(?<=Content-Length>)[^<]+' "./testazure.xml"))
+
+    teststorage=0
+    for i in ${!blobs[*]}
+    do
+        echo "$i" "${blobs[$i]}"
+        echo "File: ${blobs[$i]} size: ${bloblens[$i]}"
+        teststorage=1
+    done
+    if [[ "$teststorage" == "0" ]] ; then
+        echo "Azure Storage Test failed - check files https://${AV_STORAGENAME}.blob.core.windows.net/${AV_CONTAINERNAME}?${AV_SASTOKEN}"
+        kill %1
+        exit 0
+    fi
+    echo "Testing output on Azure Storage successful"
     #jobs
     kill %1
     echo "TESTS SUCCESSFUL"
