ad33f25940ddd6f5ccf5913e92be0989a01598c5
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-03-04 23:38:52 +0100

    update av-ffmpeg ubuntu

--- envs/container/docker/av-rtmp-source/alpine/docker-entrypoint.sh
+++ envs/container/docker/av-rtmp-source/alpine/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 while [ : ]
 do
 echo "ffmpeg -v verbose  -re -stream_loop -1 -i /camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb   -f flv $RTMP_URL"
+ffmpeg -version
 ffmpeg -v verbose  -re -stream_loop -1 -i /camera-300s.mkv -codec copy -bsf:v h264_mp4toannexb   -f flv $RTMP_URL
 sleep 5
 done
