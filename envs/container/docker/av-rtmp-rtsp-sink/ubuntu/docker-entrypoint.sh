cfa8ae7838c932b092cc120b1447edd969fbc513
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-03-17 11:52:41 +0100

    update alpine container to disable rtmp ingestion with rtsp server

--- envs/container/docker/av-rtmp-rtsp-sink/ubuntu/docker-entrypoint.sh
+++ envs/container/docker/av-rtmp-rtsp-sink/ubuntu/docker-entrypoint.sh
@@ -154,7 +154,10 @@ while sleep 60; do
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
   if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
+    if [[ $PROCESS_1_STATUS -ne 0 ]] ; then echo "nginx process stopped"; fi
+    if [[ $PROCESS_2_STATUS -ne 0 ]] ; then echo "rtsp-simple-server process stopped"; fi
+    if [[ $PROCESS_3_STATUS -ne 0 ]] ; then echo "ffmpeg process stopped"; fi
+    echo "One of the processes has already exited. Stopping the container"
     exit 1
   fi
 done
