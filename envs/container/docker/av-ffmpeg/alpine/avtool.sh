9c885c144e25d9992fc8893c45eb173305d469b1
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-03-19 10:27:32 +0100

    update avtool.sh to support pre-requisite installation on Ubuntu 20.04

--- envs/container/docker/av-ffmpeg/alpine/avtool.sh
+++ envs/container/docker/av-ffmpeg/alpine/avtool.sh
@@ -137,7 +137,7 @@ if [[ "${action}" == "install" ]] ; then
         stable"
     sudo apt-get -y update
     checkAVError
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io    
+    sudo apt-get -y install docker.io    
     checkAVError
     sudo groupadd docker || true
     sudo usermod -aG docker ${USER} || true
