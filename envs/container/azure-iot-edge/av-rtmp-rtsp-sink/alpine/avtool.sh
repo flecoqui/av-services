b56fe977ec8e788f9ea480fb713bf6e4f426f311
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-03-21 01:14:56 +0100

    code complete to deploy Iot Edge with LVA modules and RTMP to RTSP container

--- envs/container/azure-iot-edge/av-rtmp-rtsp-sink/alpine/avtool.sh
+++ envs/container/azure-iot-edge/av-rtmp-rtsp-sink/alpine/avtool.sh
@@ -271,7 +271,7 @@ if [[ "${action}" == "deploy" ]] ; then
         echo "Error while building the image"
         exit 1
     fi    
-    az acr repository  untag  -n "${CONTAINER_REGISTRY}" --image ${AV_IMAGE_FOLDER}/${AV_IMAGE_NAME}:latest 
+    az acr repository  untag  -n "${CONTAINER_REGISTRY}" --image ${AV_IMAGE_FOLDER}/${AV_IMAGE_NAME}:latest || true
     az acr  import  -n "${CONTAINER_REGISTRY}" --source ${AV_IMAGE_FOLDER}/${AV_IMAGE_NAME}:${tagID} --image ${AV_IMAGE_FOLDER}/${AV_IMAGE_NAME}:latest
     
     echo
