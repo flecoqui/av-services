29c429e45e5064aa0b7200da34da8aa79e9970fe
Frédéric Le Coquil <flecoqui@microsoft.com>
2021-03-28 14:29:35 +0200

    update .Net50 SDK instalaltion

--- envs/container/azure-iot-edge/av-rtmp-rtsp-sink/alpine/avtool.sh
+++ envs/container/azure-iot-edge/av-rtmp-rtsp-sink/alpine/avtool.sh
@@ -341,10 +341,8 @@ if [[ "${action}" == "install" ]] ; then
     echo "Installing .Net 5.0 SDK "
     wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
-    sudo apt-get update; \
-    sudo apt-get install -y apt-transport-https && \
-    sudo apt-get update && \
-    #sudo apt-get install -y dotnet-sdk-5.0
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https 
     sudo apt-get install -y dotnet-sdk-5.0
     dotnet restore ../../../../../src/lvatool
     dotnet build ../../../../../src/lvatool
